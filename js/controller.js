4374da7a49c7b5dd175c50bb293cd60a7eb56eb6
Leandro Marcucci <leanucci@gmail.com>
2012-08-02 16:39:26 -0300

    https not http

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,7 @@
 // Controllers
 
 function ReposCtrl($scope, $http) {
-  $http.get("http://api.github.com/users/leanucci/repos").success(function(data) {
+  $http.get("https://api.github.com/users/leanucci/repos").success(function(data) {
     $scope.repos = data;
   });
 }
