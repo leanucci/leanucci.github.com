bae005197d6d10339466809c569b28abc200a521
Leandro Marcucci <leanucci@gmail.com>
2012-08-07 14:13:32 -0300

    Repos iterator with a couple filters

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,22 @@
-'use strict';
+angular.module("Leanucci", []).
+  filter('capitalize', function() {
+    return function(input) {
+      var out = input.slice(1);
+      var first = input.charAt(0).toUpperCase();
+      return first + out;
+    }
+  }).
+  filter('default', function() {
+    return function(input, defaultVal) {
+      var out = (input.length < 1) ? defaultVal : input;
+      return out;
+    }
+  });
 
-// Controllers
 
 function ReposCtrl($scope, $http) {
-  $http.get("https://api.github.com/users/leanucci/repos").success(function(data) {
-    $scope.repos = data;
-  });
+  $http.jsonp("https://api.github.com/users/leanucci/repos?callback=JSON_CALLBACK").
+    success(function(data) {
+      $scope.repos = data.data;
+    });
 }
